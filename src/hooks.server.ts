34be21ed068277f74216a91c3dea29140b35fa56
daim43012 <vgatsko.21@gmail.com>
2025-12-30 02:15:13 +0300

    auth(core): resolve user globally in hooks.server

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,1 +1,20 @@
 import 'dotenv/config';
+import type { Handle } from "@sveltejs/kit";
+import { verifyJwt } from "$lib/jwt";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const token = event.cookies.get("auth_token");
+
+  if (token) {
+    try {
+      event.locals.user = await verifyJwt(token);
+    } catch {
+      event.cookies.delete("auth_token", { path: "/" });
+      event.locals.user = null;
+    }
+  } else {
+    event.locals.user = null;
+  }
+
+  return resolve(event);
+};
