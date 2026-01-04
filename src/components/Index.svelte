520c8f7a43cc138ffa55e849cdb14a8b6375a458
daim43012 <vgatsko.21@gmail.com>
2026-01-04 19:57:24 +0300

    ui(public): wire HomePosts with page data

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import Hero from "../components/index/Hero.svelte";
+  import HomePosts from "./index/HomePosts.svelte";
   import PathSelector from "./index/PathSelector.svelte";
+
+  export let data: any;
 </script>
 
 <Hero />
 <PathSelector />
-
+<HomePosts {data} />
