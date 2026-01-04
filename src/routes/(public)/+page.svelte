9b5b8244868daefe8643a21510626321b5dfb245
daim43012 <vgatsko.21@gmail.com>
2026-01-04 19:57:09 +0300

    routes(public): pass page data to Index

--- src/routes/(public)/+page.svelte
+++ src/routes/(public)/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
+  import type { PageData } from "./$types";
   import Index from "../../components/Index.svelte";
 
+  export let data: PageData;
 </script>
 
-<Index />
+<Index {data} />
