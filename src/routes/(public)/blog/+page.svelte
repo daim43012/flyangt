380328b2bc525fcd076c8589703f15b2861ab593
daim43012 <vgatsko.21@gmail.com>
2026-01-04 19:57:45 +0300

    routes(public): mount Blog page component

---
+++ src/routes/(public)/blog/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Blog from "../../../components/publicPages/Blog.svelte";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<Blog {data} />
