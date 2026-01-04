0b3aa5598131851d0f71cecb04b5e47f16c69922
daim43012 <vgatsko.21@gmail.com>
2026-01-04 22:27:36 +0300

    ui(index): add roadmap section

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -2,10 +2,12 @@
   import Hero from "../components/index/Hero.svelte";
   import HomePosts from "./index/HomePosts.svelte";
   import PathSelector from "./index/PathSelector.svelte";
+  import Roadmap from "./index/Roadmap.svelte";
 
   export let data: any;
 </script>
 
 <Hero />
 <PathSelector />
+<Roadmap />
 <HomePosts {data} />
