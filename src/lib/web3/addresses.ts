5859e0d2b1a0c0069e3d906a567e1a8ebb9aad8a
daim43012 <vgatsko.21@gmail.com>
2026-02-20 17:32:22 +0300

    web3: add addresses config

---
+++ src/lib/web3/addresses.ts
@@ -0,0 +1,14 @@
+export const ADDRESSES = {
+  localhost: {
+    presale: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+    usdt: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    usdc: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    presaleFromBlock: 0,
+  },
+  polygon: {
+    presale: "0x5924335Dd80C97C3b7459bbF7Fd553291a730951",
+    usdt: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
+    usdc: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
+    presaleFromBlock: 0,
+  },
+} as const;
