65d76f16a92ccbcdb1a93dc58517561ed6be19b5
daim43012 <vgatsko.21@gmail.com>
2026-02-20 17:32:22 +0300

    web3: add erc20 abi

---
+++ src/lib/web3/abi/erc20.abi.ts
@@ -0,0 +1,6 @@
+export const ERC20_ABI = [
+  "function decimals() view returns (uint8)",
+  "function balanceOf(address) view returns (uint256)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function approve(address spender, uint256 amount) returns (bool)",
+] as const;
