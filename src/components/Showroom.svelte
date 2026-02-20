a11d643ee91b81938cdbe96bc0ec63df28e1fb29
daim43012 <vgatsko.21@gmail.com>
2026-02-20 16:39:06 +0300

    ui: update Showroom component

---
+++ src/components/Showroom.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Hero from "./showroom/Hero.svelte";
+  import Model from "./showroom/Model.svelte";
+  import Ownership from "./showroom/Ownership.svelte";
+  import Configurator from "./showroom/Configurator.svelte";
+  import Compliance from "./showroom/Compliance.svelte";
+  import Process from "./showroom/Process.svelte";
+  import Cta from "./showroom/Cta.svelte";
+</script>
+
+<Hero />
+<Model />
+<Ownership />
+<Configurator />
+<Compliance />
+<Process />
+<Cta />
