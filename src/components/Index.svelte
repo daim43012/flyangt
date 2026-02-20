bc41436786c46fff1e27fb4822cacafc7e81f85c
daim43012 <vgatsko.21@gmail.com>
2026-02-20 16:38:55 +0300

    ui: update index component

--- src/components/Index.svelte
+++ src/components/Index.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
   import Hero from "../components/index/Hero.svelte";
+  import TestBlender from "./App/testBlender.svelte";
+  import CyprusHub from "./index/CyprusHub.svelte";
   import HomePosts from "./index/HomePosts.svelte";
   import PathSelector from "./index/PathSelector.svelte";
-  import Roadmap from "./index/Roadmap.svelte";
+  import PerformanceComparisonSection from "./index/PerformanceComparisonSection.svelte";
+  import Plane from "./index/Plane.svelte";
+  import Why from "./index/Why.svelte";
+  import PlanePath from "./PlanePath.svelte";
+  import FlightRoutesMap from "./publicPages/FlightRoutesMap.svelte";
+  import Team from "./publicPages/Team.svelte";
 
   export let data: any;
 </script>
 
 <Hero />
+<!-- <Plane /> -->
+<Why/>
+<CyprusHub/>
+<PerformanceComparisonSection/>
+<FlightRoutesMap/>
 <PathSelector />
-<Roadmap />
+<PlanePath />
+<Team/>
 <HomePosts {data} />
+
+<!-- <TestBlender /> -->
