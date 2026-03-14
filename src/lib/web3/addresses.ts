cf4871edde480115c013a0e86e1f7219d6b6b539
daim43012 <vgatsko.21@gmail.com>
2026-03-14 21:45:59 +0300

    feat(web3): update addresses

--- src/lib/web3/addresses.ts
+++ src/lib/web3/addresses.ts
@@ -6,9 +6,9 @@ export const ADDRESSES = {
     presaleFromBlock: 0,
   },
   polygon: {
-    presale: "0x5924335Dd80C97C3b7459bbF7Fd553291a730951",
+    presale: "0xdd03b252829A3ebE19F03cf5B6fa033b3DB3Bed0",
     usdt: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
     usdc: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359",
-    presaleFromBlock: 0,
+    presaleFromBlock: 82858156,
   },
 } as const;
