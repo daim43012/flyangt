e71b270ce4f21d5906f59f852afda8b0c43dfb50
daim43012 <vgatsko.21@gmail.com>
2026-03-14 21:45:48 +0300

    config: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+		define: {
+			'process.env.POLYGON_RPC_URL': JSON.stringify(env.POLYGON_RPC_URL || ''),
+		},
+	};
 });
