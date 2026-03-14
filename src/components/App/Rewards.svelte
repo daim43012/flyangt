e1bf032666b272cbbf987d2d81b8569d1f9c7bed
daim43012 <vgatsko.21@gmail.com>
2026-03-14 21:46:12 +0300

    feat(rewards): update rewards page

--- src/components/App/Rewards.svelte
+++ src/components/App/Rewards.svelte
@@ -2,13 +2,17 @@
   import Benefits from "./rewards/Benefits.svelte";
   import Hero from "./rewards/Hero.svelte";
   import RewardsStation from "./rewards/RewardsStation.svelte";
+  import RewardsTimer from "./rewards/RewardsTimer.svelte";
 </script>
 
 <section class="rewards">
   <div class="main-col">
-    <Hero />
-    <Benefits />
-    <RewardsStation />
+    <div class="top">
+      <div data-tour-page="rewards-timer"><RewardsTimer /></div>
+      <div data-tour-page="rewards-yield"><Hero /></div>
+    </div>
+    <div data-tour-page="rewards-terms"><Benefits /></div>
+    <div data-tour-page="rewards-station"><RewardsStation /></div>
   </div>
 </section>
 
@@ -27,8 +31,15 @@
     min-width: 0;
   }
 
-  @media (max-width: 1100px) {
-    .rewards {
+  .top {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 18px;
+    align-items: stretch;
+  }
+
+  @media (max-width: 980px) {
+    .top {
       grid-template-columns: 1fr;
     }
   }
